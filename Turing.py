5502fdf65feba241de3e6c5d6707cdfd8ab96b53
HarshTheSharma <71364679+HarshTheSharma@users.noreply.github.com>
2024-10-24 20:40:32 -0700

    Turing.py Variable Name Consistency
    Updated Turing.py For Variable Name Consistency

--- Turing.py
+++ Turing.py
@@ -23,10 +23,10 @@ def Server():
     
     # Accept connections from n peers
     for _ in range(n):
-        client_socket, addr = serverSocket.accept()
-        print(f"Accepted connection from {addr}")
-        peers.append((client_socket, addr))
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, peers))
+        clientSocket, clientAddr = serverSocket.accept()
+        print(f"Accepted connection from {clientAddr}")
+        peers.append((clientSocket, clientAddr))
+        client_handler = threading.Thread(target=handle_client, args=(clientSocket, peers))
         client_handler.start()
     
     # Send a message to 1/4 of the peers with the IP address and port of a random peer. This means half of the peers will be assigned another human to talk to.
@@ -56,52 +56,52 @@ def Server():
 
 
 class Client:
-    def __init__(self, server_ip, server_port):
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.peer_ip = None
-        self.peer_port = None
+    def __init__(self, serverIP, serverPort):
+        self.serverIP = serverIP
+        self.serverPort = serverPort
+        self.peerIP = None
+        self.peerPort = None
         self.priority = None
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     def connect_to_server(self):
-        self.client_socket.connect((self.server_ip, self.server_port))
-        print(f"Connected to server at {self.server_ip}:{self.server_port}")
+        self.clientSocket.connect((self.serverIP, self.serverPort))
+        print(f"Connected to server at {self.serverIP}:{self.serverPort}")
     
     def receive_message(self):
         while(not message):
-            message = self.client_socket.recv(1024).decode()
-            peer_info, self.priority = message.split(',')
-            self.peer_ip, self.peer_port = peer_info.split(':')
-            self.peer_port = int(self.peer_port)
+            message = self.clientSocket.recv(1024).decode()
+            peerInfo, self.priority = message.split(',')
+            self.peerIP, self.peerPort = peerInfo.split(':')
+            self.peerPort = int(self.peerPort)
             self.priority = int(self.priority)
             print(f"Received message: {message}")
-            print(f"Peer IP: {self.peer_ip}, Peer Port: {self.peer_port}, Priority: {self.priority}")
+            print(f"Peer IP: {self.peerIP}, Peer Port: {self.peerPort}, Priority: {self.priority}")
     
     def communicate_with_peer(self):
-        peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        peer_socket.connect((self.peer_ip, self.peer_port))
+        peerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        peerSocket.connect((self.peerIP, self.peerPort))
         
         if self.priority == 1:
-            self.send_message(peer_socket)
-            self.receive_message_from_peer(peer_socket)
+            self.send_message(peerSocket)
+            self.receive_message_from_peer(peerSocket)
         else:
-            self.receive_message_from_peer(peer_socket)
-            self.send_message(peer_socket)
+            self.receive_message_from_peer(peerSocket)
+            self.send_message(peerSocket)
         
-        peer_socket.close()
+        peerSocket.close()
     
-    def send_message(self, peer_socket):
+    def send_message(self, peerSocket):
         message = "Hello from client!"
-        peer_socket.send(message.encode())
+        peerSocket.send(message.encode())
         print(f"Sent message: {message}")
     
-    def receive_message_from_peer(self, peer_socket):
-        message = peer_socket.recv(1024).decode()
+    def receive_message_from_peer(self, peerSocket):
+        message = peerSocket.recv(1024).decode()
         print(f"Received message: {message}")
     
     def start(self):
         self.connect_to_server()
         self.receive_message()
         self.communicate_with_peer()
-        self.client_socket.close()
+        self.clientSocket.close()
